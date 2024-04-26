cf8efbd961d88fe87b157040293ef9199f205d86
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-04-26 07:49:50 -0500

    Revert "Remove unnecessary quotes"
    This reverts commit 04c5c65a4371cb9cfa53a8b4423ea32aa0c95d9b.

--- back-end/database/init.sql
+++ back-end/database/init.sql
@@ -1,3 +1,3 @@
-CREATE USER pinturillo_db_admin WITH ENCRYPTED PASSWORD pinturillo_db_admin;
+CREATE USER pinturillo_db_admin WITH ENCRYPTED PASSWORD "pinturillo_db_admin";
 
 CREATE DATABASE pinturillo_db WITH OWNER pinturillo_db_admin ENCODING 'UTF8';
