814f1ddcb04d828fa0af1fd808fd58b49d8ebf36
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-04-30 06:33:04 -0500

    Add data-source

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { AppDataSource } from "./data-source";
 
 // configures dotenv to work in your application
 dotenv.config();
@@ -11,9 +12,12 @@ app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
 }); 
 
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  // gracefully handle error
-  throw new Error(error.message);
-});
+
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(3000, () => {
+      console.log("Server is running on http://localhost:" + PORT);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
