d9587769d6be79b66a81f34f8035d389fe5094ed
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-04-30 19:21:37 -0500

    Fix type bugs

--- back-end/src/repositories/common/repository.generic.ts
+++ back-end/src/repositories/common/repository.generic.ts
@@ -1,18 +1,18 @@
 import { EntityBase } from "../../entities/common";
 import { IRepository } from "./repository.interface";
-import { Repository }  from "typeorm";
+import { FindOptionsWhere, Repository }  from "typeorm";
 
 export class GenericRepository<T extends EntityBase> implements IRepository<T>{
-    private _repository:Repository;
+    private _repository:Repository<T>;
 
-    constructor(repository:Repository){
+    constructor(repository:Repository<T>){
         this._repository = repository;
     }
 
-    async getOneBy(options: Object): Promise<T | null> {
+    async getOneBy(options: FindOptionsWhere<T> | FindOptionsWhere<T>[]): Promise<T | null> {
        return this._repository.findOneBy(options);
     }
-    async getBy(options: Object): Promise<T[]> {
+    async getBy(options: FindOptionsWhere<T> | FindOptionsWhere<T>[]): Promise<T[]> {
         return this._repository.findBy(options);
     }
     async getAll(): Promise<T[]> {
