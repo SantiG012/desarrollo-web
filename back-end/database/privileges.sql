970be5b34a4d836f28b31963d8715ba6967bd6be
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-04 16:46:04 -0500

    Add more privileges

--- back-end/database/privileges.sql
+++ back-end/database/privileges.sql
@@ -2,3 +2,8 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON palabra TO pinturillo_db_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON categoria TO pinturillo_db_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON palabras_por_categoria TO pinturillo_db_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON sala_de_juego TO pinturillo_db_admin;
+
+GRANT USAGE, SELECT ON SEQUENCE palabra_id_seq TO pinturillo_db_admin;
+GRANT USAGE, SELECT ON SEQUENCE categoria_id_seq TO pinturillo_db_admin;
+GRANT USAGE, SELECT ON SEQUENCE palabras_por_categoria_id_seq TO pinturillo_db_admin;
+GRANT USAGE, SELECT ON SEQUENCE sala_de_juego_id_seq TO pinturillo_db_admin;
