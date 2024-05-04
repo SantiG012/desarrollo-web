c9a27b02ad5e851621cb754f907396963f16bc26
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-04 14:35:00 -0500

    Refactor a method

--- back-end/src/repositories/common/repository.generic.ts
+++ back-end/src/repositories/common/repository.generic.ts
@@ -1,6 +1,6 @@
 import { EntityBase } from "../../entities/common";
 import { IRepository } from "./repository.interface";
-import { FindOptionsWhere, Repository }  from "typeorm";
+import { FindManyOptions, FindOptionsWhere, Repository }  from "typeorm";
 
 export class GenericRepository<T extends EntityBase> implements IRepository<T>{
     private _repository:Repository<T>;
@@ -15,8 +15,8 @@ export class GenericRepository<T extends EntityBase> implements IRepository<T>{
     async getBy(options: FindOptionsWhere<T> | FindOptionsWhere<T>[]): Promise<T[]> {
         return this._repository.findBy(options);
     }
-    async getAll(): Promise<T[]> {
-        return this._repository.find();
+    async get(options?: FindManyOptions<T> | undefined): Promise<T[]> {
+        return this._repository.find(options);
     }
     async create(entity: T): Promise<T> {
         return this._repository.save(entity);
--- back-end/src/repositories/common/repository.interface.ts
+++ back-end/src/repositories/common/repository.interface.ts
@@ -1,9 +1,10 @@
+import { FindManyOptions, FindOptionsWhere } from "typeorm";
 import { EntityBase } from "../../entities/common";
 
 export interface IRepository<T extends EntityBase>{
-    getOneBy(options:Object): Promise<T | null>;
-    getBy(options:Object): Promise<T[]>;
-    getAll():Promise<T[]>;
+    getOneBy(options: FindOptionsWhere<T> | FindOptionsWhere<T>[]): Promise<T | null>;
+    getBy(options: FindOptionsWhere<T> | FindOptionsWhere<T>[]): Promise<T[]>;
+    get(options?: FindManyOptions<T> | undefined):Promise<T[]>;
     create(entity:T):Promise<T>;
     update(entity:T):Promise<T>;
     delete(id:number):Promise<void>;   
