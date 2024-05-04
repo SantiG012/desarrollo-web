d29f4bac9494a48a2e9d6cf23318db7fec371a6d
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-04 14:32:54 -0500

    Fix a bug

--- back-end/src/entities/words-by-category.entity.ts
+++ back-end/src/entities/words-by-category.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne } from "typeorm";
+import { Entity, JoinColumn, ManyToOne } from "typeorm";
 import { EntityBase } from "./common";
 import { Category } from "./category.entity";
 import { Word } from "./word.entity";
@@ -6,8 +6,10 @@ import { Word } from "./word.entity";
 @Entity({name:'palabras_por_categoria'})
 export class WordsByCategory extends EntityBase {
     @ManyToOne(()=> Category, (category)=>category.wordsByCategory)
+    @JoinColumn({name:'id_categoria'})
     category:Category;
 
+    @JoinColumn({name:'id_palabra'})
     @ManyToOne(()=> Word, (word)=>word.wordsByCategory)
     word:Word;
 }
