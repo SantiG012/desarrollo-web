6d9946f97ca4d64040a3affd908d5ea06aebb9c0
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 18:30:28 -0500

    Fix a type bug

--- back-end/src/use-cases/index.ts
+++ back-end/src/use-cases/index.ts
@@ -1,2 +1,5 @@
 export * from './word.use-cases';
 export * from './category.use-cases';
+export * from './words-by-category.use-cases';
+export * from './games.use-cases';
+export * from './play-room.use-cases';
