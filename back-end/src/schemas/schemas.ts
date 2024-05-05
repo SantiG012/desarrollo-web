9bfd28c58dbf2d44f3c05862b76688b5afb96910
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 17:55:27 -0500

    Refactor schemas

--- back-end/src/schemas/schemas.ts
+++ back-end/src/schemas/schemas.ts
@@ -1,39 +1,30 @@
 import Joi, {ObjectSchema } from "joi";
 
-const createWordSchema: ObjectSchema = Joi.object().keys({
-    text: Joi.string().required().max(255),
-});
-
-const editWordSchema: ObjectSchema = Joi.object().keys({
+const wordSchema: ObjectSchema = Joi.object().keys({
     id: Joi.number().required(),
-    text: Joi.string().max(255),
+    text: Joi.string().required().max(100),
 });
 
-const createCategorySchema: ObjectSchema = Joi.object().keys({
+const categorySchema: ObjectSchema = Joi.object().keys({
+    id: Joi.number().required(),
     name: Joi.string().required().max(100),
 });
 
-const editCategorySchema: ObjectSchema = Joi.object().keys({
+const wordsByCategorySchema: ObjectSchema = Joi.object().keys({
     id: Joi.number().required(),
-    name: Joi.string().max(100),
+    category: categorySchema.required(),
+    word: Joi.array().items(wordSchema).required(),
 });
 
-const createWordsByCategorySchema: ObjectSchema = Joi.object().keys({
-    category: createCategorySchema.required(),
-    word: Joi.array().items(createWordSchema).required(),
-});
-
-const createPlayRoomSchema: ObjectSchema = Joi.object().keys({
+const playRoomSchema: ObjectSchema = Joi.object().keys({
     name: Joi.string().required().max(100),
     state: Joi.string().required().max(20),
     categoryId: Joi.number().required(),
 });
 
 export default {
-    createWordSchema,
-    editWordSchema,
-    createCategorySchema,
-    editCategorySchema,
-    createWordsByCategorySchema,
-    createPlayRoomSchema,
-};
+    "/words": wordSchema,
+    "/categories": categorySchema,
+    "/words-by-category": wordsByCategorySchema,
+    "/play-rooms": playRoomSchema,
+} as {[key: string]: ObjectSchema};
