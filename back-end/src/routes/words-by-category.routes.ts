0745ac27a963f5e2da443ac05384aac54d913e70
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 18:29:41 -0500

    Add words by category routes

---
+++ back-end/src/routes/words-by-category.routes.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from "express";
+import schemaValidator from "../middleware/schema.validator";
+import { WordsByCategoryUseCases } from "../use-cases";
+
+const router = Router();
+
+const wordsByCategoryUseCases:WordsByCategoryUseCases = new WordsByCategoryUseCases();
+
+router.post("/", 
+    schemaValidator("wordsByCategory"),
+    async (req: Request, res: Response) => {
+        const wordsByCategory = req.body;
+        res.status(201).send(await wordsByCategoryUseCases.associateWordWithCategory(wordsByCategory));
+    }
+);
+
+router.delete("/",
+    schemaValidator("wordsByCategory"),
+    async (req: Request, res: Response) => {
+       const wordsByCategory = req.body;
+       res.status(200).send(await wordsByCategoryUseCases.disassociateWordWithCategory(wordsByCategory));
+    }
+);
