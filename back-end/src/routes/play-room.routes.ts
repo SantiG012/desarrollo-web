51c9ddef6504757f8e23ac1239e7aa5b6c195eec
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 18:53:08 -0500

    Complete the play room routes

--- back-end/src/routes/play-room.routes.ts
+++ back-end/src/routes/play-room.routes.ts
@@ -10,16 +10,16 @@ router.get("/",
     async (req: Request, res: Response) => {
         const state = req.query.state as string;
         const categoryId = req.query.categoryId as string;
-        //TODO: Implemente the logic to filter by state and categoryId
-        res.status(200).send(await playRoomUseCases);
+
+        res.status(200).send(await playRoomUseCases.getPlayRooms(state, parseInt(categoryId)));
     }
 );
 
 router.get("/:id/words",
     async (req: Request, res: Response) => {
         const id = parseInt(req.params.id);
-        //TODO: Implemente the logic to get the words of a play room
-        res.status(200).send(await playRoomUseCases);
+
+        res.status(200).send(await playRoomUseCases.getPlayRoomsWords(id));
     }
 );
 
