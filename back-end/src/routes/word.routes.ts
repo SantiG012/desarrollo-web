47d8cf18a96419471c05668bb8eb37019476a21a
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 18:00:32 -0500

    Add a use case to a router

--- back-end/src/routes/word.routes.ts
+++ back-end/src/routes/word.routes.ts
@@ -1,13 +1,25 @@
 import { Router, Request, Response } from "express";
+import schemaValidator from "../middleware/schema.validator";
+import { WordUseCases } from "../use-cases";
 
 const router = Router();
 
-router.post("/", (req: Request, res: Response) => {
-    res.send("Word created");
-});
+const wordUseCases:WordUseCases = new WordUseCases();
 
-router.put("/", (req: Request, res: Response) => {
-    res.send("Word updated");
-});
+router.post("/", 
+    schemaValidator("word"),
+    async (req: Request, res: Response) => {
+        const word = req.body;
+        res.status(201).send(await wordUseCases.addWord(word));
+    }
+);
+
+router.put("/", 
+    schemaValidator("word"),
+    (req: Request, res: Response) => {
+        const word = req.body;
+        res.status(200).send(wordUseCases.editWord(word));
+    }   
+);
 
 export default router;
