05384dfe6ea6a949754e08a8253f1ee6cb355577
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 21:53:45 -0500

    Add try catch

--- back-end/src/routes/play-room.routes.ts
+++ back-end/src/routes/play-room.routes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import schemaValidator from "../middleware/schema.validator";
 import { PlayRoomUseCases } from "../use-cases";
 
@@ -6,36 +6,45 @@ const router = Router();
 
 const playRoomUseCases:PlayRoomUseCases = new PlayRoomUseCases();
 
-router.get("/",
-    async (req: Request, res: Response) => {
-        const state = req.query.state as string;
-        const categoryId = req.query.categoryId as string;
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+    const state = req.query.state as string;
+    const categoryId = req.query.categoryId as string;
 
+    try {
         res.status(200).send(await playRoomUseCases.getPlayRooms(state, parseInt(categoryId)));
+    } catch (error) {
+        next(error);
     }
-);
+});
 
-router.get("/:id/words",
-    async (req: Request, res: Response) => {
-        const id = parseInt(req.params.id);
-
+router.get("/:id/words", async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    
+    try {
         res.status(200).send(await playRoomUseCases.getPlayRoomsWords(id));
+    } catch (error) {
+        next(error);
     }
-);
+});
 
-router.post("/", 
-    schemaValidator("/play-rooms"),
-    async (req: Request, res: Response) => {
-        const playRoom = req.body;
+router.post("/", schemaValidator("/play-rooms"), async (req: Request, res: Response, next: NextFunction) => {
+    const playRoom = req.body;
+
+    try {
         res.status(201).send(await playRoomUseCases.createPlayRoom(playRoom));
+    } catch (error) {
+        next(error);
     }
-);
+});
 
-router.delete("/:id",
-    async (req: Request, res: Response) => {
-        const id = parseInt(req.params.id);
+router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    try {
         res.status(200).send(await playRoomUseCases.deletePlayRoom(id));
+    } catch (error) {
+        next(error);
     }
-);
+});
+
 
 export default router;
