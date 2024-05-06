2cb5d6824c14141a2ad4d7d5f3bc501a0ab93931
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-05 21:10:41 -0500

    Add delete on cascade

--- back-end/database/tables.sql
+++ back-end/database/tables.sql
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS palabras_por_categoria (
     id SERIAL PRIMARY KEY,
     id_palabra INT,
     id_categoria INT,
-    FOREIGN KEY (id_palabra) REFERENCES palabra(id),
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id),
+    FOREIGN KEY (id_palabra) REFERENCES palabra(id)  ON DELETE CASCADE,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)  ON DELETE CASCADE,
     UNIQUE(id_palabra, id_categoria)
 );
 
@@ -24,6 +24,6 @@ CREATE TABLE IF NOT EXISTS sala_de_juego (
     nombre VARCHAR(100),
     id_categoria INT,
     estado VARCHAR(20),
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)  ON DELETE CASCADE,
     UNIQUE (nombre)
 );
