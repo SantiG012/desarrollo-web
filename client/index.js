9a30ccba5df586c4e5a4303abdafa5533355a728
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-16 19:32:04 -0500

    Add a new feature

--- client/index.js
+++ client/index.js
@@ -1,11 +1,17 @@
-const socket = new WebSocket("ws://localhost:3000/ws/room/1?userId=s1r&name=santi&avatar=s");
 const textBox = document.getElementById("input");
 const sendButton = document.getElementsByTagName("button")[0];
+const url = window.location.href; // Get the current URL
+const urlObj = new URL(url);  // Create a URL object
+const queryParams = new URLSearchParams(urlObj.search); // Get the query parameters
+const { userId, userName, userAvatar } = Object.fromEntries(queryParams.entries()); // Get the userId, name, and avatar from the query parameters
+const socket = new WebSocket(`ws://localhost:3000/ws/room/1?userId=${userId}&name=${userName}&avatar=${userAvatar}`);
 
 
-
-
-sendButton.addEventListener("click",sendMessage);
+sendButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    sendMessage();
+    textBox.value = "";
+});
 
 function sendMessage(){
     const message = textBox.value
