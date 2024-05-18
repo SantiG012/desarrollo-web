5d89c09fcf31d1838b1d5a8deea4f563fc3e52af
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-18 06:24:19 -0500

    Refactor an interface

--- back-end/src/interfaces/communication.interface.ts
+++ back-end/src/interfaces/communication.interface.ts
@@ -1,7 +1,9 @@
 import { GameEventTypes } from "../Enums/game-event-types.enum"
 import { ChatMessagePayload } from "./chat-message-payload.interface"
+import { ResultsPayload } from "./results-payload.interface"
 
 export interface Communication {
     gameEventType: GameEventTypes,
-    payload: ChatMessagePayload | any
+    chatMessagePayload?: ChatMessagePayload,
+    resultsPayload?: ResultsPayload[]
 }
