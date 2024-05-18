c7e4baaf7402d0a004ed99de77dba148462fa26a
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-18 05:17:04 -0500

    Add a payload

---
+++ back-end/src/interfaces/chat-message-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface ChatMessagePayload {
+    message: string;
+    senderId: string;
+    senderName: string;
+}
--- back-end/src/interfaces/index.ts
+++ back-end/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './player.interface';
 export * from './words-by-category';
 export * from './communication.interface';
+export * from './chat-message-payload.interface';
