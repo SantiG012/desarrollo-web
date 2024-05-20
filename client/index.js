33f7af192e8bf6174f9589271008259ea1b71be5
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-05-19 19:32:01 -0500

    Add logic to handle messages

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 const textBox = document.getElementById("input");
 const sendButton = document.getElementsByTagName("button")[0];
+const messages = document.getElementById("messages");
 const url = window.location.href; // Get the current URL
 const urlObj = new URL(url);  // Create a URL object
 const queryParams = new URLSearchParams(urlObj.search); // Get the query parameters
@@ -9,32 +10,94 @@ const socket = new WebSocket(`ws://localhost:3000/ws/room/1?userId=${userId}&nam
 
 sendButton.addEventListener("click",(e)=>{
     e.preventDefault();
-    sendMessage(EventType.CHAT_MESSAGE);
+    sendMessage(GameEventType.CHAT_MESSAGE);
     textBox.value = "";
 });
 
-function sendMessage(eventType){
+function sendMessage(gameEventType){
     const message = textBox.value
     socket.send(JSON.stringify({
-        eventType,
-        payload: {
+        gameEventType,
+        chatMessagePayload: {
             message,
+            senderId: userId,
+            senderName: userName,
         }
     }));
 }
 
 
+function requestEntryToGameRoom(){
+    socket.send(JSON.stringify({
+        gameEventType: GameEventType.JOIN_GAME,
+        payload: {}
+    }));
+}
+
+function handleEventType(communicationInterface){
+    switch(communicationInterface.gameEventType){
+        case GameEventType.ROUND_NOTIFICATION:
+            handleRoundNotification(communicationInterface.roundNotificationPayload);
+            break;
+        case GameEventType.CHAT_MESSAGE:
+            handleChatMessage(communicationInterface.chatMessagePayload);
+            break;
+        case GameEventType.FINISH_GAME:
+            handleFinishGame();
+            break;
+    }
+}
+
+function handleRoundNotification(payload){
+    alert(payload.message);
+}
+
+function handleChatMessage(chatMessagePayload){ 
+    const senderName = getSenderName(chatMessagePayload);
+    const item = document.createElement('li');
+    item.textContent = `${senderName}: ${chatMessagePayload.message}`
+    messages.appendChild(item);
+}
+
+function handleFinishGame(){
+    removeChatMessages();
+    //closeConnection();
+    alert("Game has finished");
+}
+
+function closeConnection(){
+    socket.close();
+}
+
+function removeChatMessages(){
+    while(messages.firstChild){
+        messages.removeChild(messages.firstChild);
+    }
+}
+
+function getSenderName(chatMessagePayload){
+    if(userId === chatMessagePayload.senderId){
+        return 'Tú';
+    }
+
+    return chatMessagePayload.senderName;
+}
 
 
 
-socket.onopen = function(event) {
-    console.log("WebSocket connection established.");
+socket.onopen = function(communicationInterface) {
+    console.log("WebSocket connection open.");
+    requestEntryToGameRoom();
 };
 
 socket.onmessage = function(event) {
-    console.log("WebSocket message received:", event.data);
+    handleEventType(JSON.parse(event.data));
 };
 
 socket.onerror = function(error) {
     console.error("WebSocket error:", error);
 };
+
+socket.onclose = function(){
+    socket.close()
+}
